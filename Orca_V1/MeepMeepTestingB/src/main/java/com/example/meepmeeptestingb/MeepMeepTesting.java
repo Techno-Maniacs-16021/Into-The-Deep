313c60908b571ebf1f35e1a8916248f4b5035351
Tanish Ghosh <tanishghosh10@gmail.com>
2024-12-13 21:28:01 -0500

    meepmeep

--- Orca_V1/MeepMeepTestingB/src/main/java/com/example/meepmeeptestingb/MeepMeepTesting.java
+++ Orca_V1/MeepMeepTestingB/src/main/java/com/example/meepmeeptestingb/MeepMeepTesting.java
@@ -1,12 +1,17 @@
 package com.example.meepmeeptestingb;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
+
     public static void main(String[] args) {
+
+        Vector2d goUpPlace = new Vector2d(-11.36,-36.59);
+
         MeepMeep meepMeep = new MeepMeep(800);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
@@ -15,15 +20,10 @@ public class MeepMeepTesting {
                 .build();
 
         myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
-                .lineToX(30)
-                .turn(Math.toRadians(90))
-                .lineToY(30)
-                .turn(Math.toRadians(90))
-                .lineToX(0)
-                .turn(Math.toRadians(90))
-                .lineToY(0)
-                .turn(Math.toRadians(90))
-                .build());
+                .setTangent(Math.toRadians(180))
+                .splineTo(goUpPlace,Math.toRadians(-45))
+                .build()
+        );
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
                 .setDarkMode(true)
