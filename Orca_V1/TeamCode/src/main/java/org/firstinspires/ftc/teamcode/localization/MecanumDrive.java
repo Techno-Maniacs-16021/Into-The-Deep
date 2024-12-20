6b077c461c6359b1dccaf50c84b96b0862ec1bde
Infant Elfrick Gnanasusairaj <infant.elfrick@gmail.com>
2024-12-19 20:01:51 -0500

    60-60 faster

--- Orca_V1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/localization/MecanumDrive.java
+++ Orca_V1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/localization/MecanumDrive.java
@@ -69,27 +69,27 @@ public class MecanumDrive {
         public double trackWidthTicks = 9.574046461401071;
 
         // feedforward parameters (in tick units)
-        public double kS = 1.5304275507890313;
-        public double kV = 0.114255431818964;
-        public double kA = 0.0325;
+        public double kS = 1.421436775802575;
+        public double kV = 0.12485493361151666;
+        public double kA = 0.03;
 
         // path profile parameters (in inches)
-        public double maxWheelVel = 50;
+        public double maxWheelVel = 60;
         public double minProfileAccel = -30;
-        public double maxProfileAccel = 50;
+        public double maxProfileAccel = 60;
 
         // turn profile parameters (in radians)
         public double maxAngVel = Math.PI; // shared with path
         public double maxAngAccel = Math.PI;
 
         // path controller gains
-        public double axialGain = 9.5;
-        public double lateralGain = 7.5;
-        public double headingGain = 5.0; // shared with turn
+        public double axialGain = 5;
+        public double lateralGain = 10;
+        public double headingGain = 4; // shared with turn
 
         public double axialVelGain = 0.0;
-        public double lateralVelGain = 0.0;
-        public double headingVelGain = 0.0; // shared with turn
+        public double lateralVelGain = 0;
+        public double headingVelGain = 0; // shared with turn
     }
 
     public static Params PARAMS = new Params();
