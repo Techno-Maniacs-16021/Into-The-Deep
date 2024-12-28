ba53e07d7103973954360b73c77126154fe09175
Infant Elfrick Gnanasusairaj <infant.elfrick@gmail.com>
2024-12-28 15:31:02 -0500

    bar centripetal, everything works perfectly

--- Orca_V1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/tuning/FollowerConstants.java
+++ Orca_V1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/tuning/FollowerConstants.java
@@ -73,7 +73,7 @@ public class FollowerConstants {
 
     // Drive PIDF coefficients
     public static CustomFilteredPIDFCoefficients drivePIDFCoefficients = new CustomFilteredPIDFCoefficients(
-            0.018,
+            0.021,
             0,
             0.00001,
             0.6,
@@ -110,7 +110,7 @@ public class FollowerConstants {
     // Decreasing this will cause the deceleration at the end of the Path to be slower, making the
     // robot slower but reducing risk of end-of-path overshoots or localization slippage.
     // This can be set individually for each Path, but this is the default.
-    public static double zeroPowerAccelerationMultiplier = 3.1;
+    public static double zeroPowerAccelerationMultiplier = 4.5;
 
 
     // When the robot is at the end of its current Path or PathChain and the velocity goes below
@@ -192,7 +192,7 @@ public class FollowerConstants {
 
     // Secondary heading error PIDF coefficients
     public static CustomPIDFCoefficients secondaryHeadingPIDFCoefficients = new CustomPIDFCoefficients(
-            2.4,
+            2,
             0,
             0.065,
             0);
